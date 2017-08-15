1404c3fc6902f8aca8b89af5ba7c55f1941d4420
Ville Ristimäki <villeristimaki@gmail.com>
2017-08-15 16:21:29 +0300

    Remove redundand `qs` import

--- assets/webpack/webpack.base.js
+++ assets/webpack/webpack.base.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const qs = require('qs');
 const argv = require('minimist')(process.argv.slice(2));
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const config = require('../../package.json').config;
